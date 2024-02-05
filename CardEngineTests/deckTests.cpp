f14b30aecbe40b52735f1aa871ec5f28ee2c91db
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-05 19:11:36 -0300

    Deck tests

---
+++ CardEngineTests/deckTests.cpp
@@ -0,0 +1,57 @@
+﻿#include "pch.h"
+
+#include "Cards/numberedCard.h"
+#include "Decks/deck.h"
+#include "Decks/IDeck.h"
+
+std::unique_ptr<Decks::IDeck> createDeck(const char* path)
+{
+    return std::make_unique<Decks::deck>(path);
+}
+
+TEST(Deck, Populate)
+{
+    const std::unique_ptr<Decks::IDeck> deck = createDeck("Data\\deck_setup.json");
+    EXPECT_EQ(80, deck->count());
+}
+
+TEST(Deck, PeekAndDequeue)
+{
+    const std::unique_ptr<Decks::IDeck> deck = createDeck("Data\\deck_setup.json");
+    Cards::ICard* first = deck->peek();
+    unsigned int count = deck->count();
+
+    EXPECT_EQ(first, deck->dequeue());
+    EXPECT_LT(count, deck->count());
+}
+
+TEST(Deck, EnqueueAndStack)
+{
+    const std::unique_ptr<Decks::IDeck> deck = createDeck("Data\\deck_setup.json");
+    Cards::ICard* first = deck->peek();
+
+    std::unique_ptr<Cards::numberedCard> card = std::make_unique<Cards::numberedCard>(1, 'b');
+
+    unsigned int count = deck->count();
+    deck->stack(card.get());
+
+    EXPECT_GT(count, deck->count());
+    EXPECT_EQ(card, deck->peek());
+    EXPECT_NE(first, deck->peek());
+
+    count = deck->count();
+    Cards::ICard* last = deck->peekLast();
+    deck->enqueue(card.get());
+
+    EXPECT_GT(count, deck->count());
+    EXPECT_LT(card, deck->peekLast());
+    EXPECT_NE(last, deck->peek());
+}
+
+TEST(Deck, Shuffle)
+{
+    const std::unique_ptr<Decks::IDeck> deck = createDeck("Data\\deck_setup.json");
+    Cards::ICard* first = deck->peek();
+    deck->shuffle();
+    EXPECT_NE(first, deck->peek());
+}
