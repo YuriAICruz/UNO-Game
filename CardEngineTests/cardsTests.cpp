ad24f70ff9d8ec7a6e82575fd86c6280d4027ed9
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-05 19:12:01 -0300

    refactor tests to use unique pointers

--- CardEngineTests/cardsTests.cpp
+++ CardEngineTests/cardsTests.cpp
@@ -1,5 +1,4 @@
 ﻿#include "pch.h"
-
 #include "Cards/drawCard.h"
 #include "Cards/numberedCard.h"
 #include "Cards/ICard.h"
@@ -8,12 +7,12 @@
 
 TEST(Cards, numberedCardComparision)
 {
-    std::vector<Cards::ICard*> cards;
-    cards.emplace_back(&Cards::numberedCard(1, 'a'));
-    cards.emplace_back(&Cards::numberedCard(2, 'a'));
-    cards.emplace_back(&Cards::numberedCard(1, 'b'));
-    cards.emplace_back(&Cards::numberedCard(2, 'b'));
-    cards.emplace_back(&Cards::numberedCard(1, 'a'));
+    std::vector<std::unique_ptr<Cards::ICard>> cards;
+    cards.emplace_back(std::make_unique<Cards::numberedCard>(1, 'a'));
+    cards.emplace_back(std::make_unique<Cards::numberedCard>(2, 'a'));
+    cards.emplace_back(std::make_unique<Cards::numberedCard>(1, 'b'));
+    cards.emplace_back(std::make_unique<Cards::numberedCard>(2, 'b'));
+    cards.emplace_back(std::make_unique<Cards::numberedCard>(1, 'a'));
 
     EXPECT_TRUE(cards[0]->sameColor(*cards[1]));
     EXPECT_TRUE(cards[2]->sameColor(*cards[3]));
@@ -43,9 +42,9 @@ TEST(Cards, drawCardComparison)
 
 TEST(Cards, reverseCardComparison)
 {
-    auto rCardA = Cards::reverseCard{'a'};
-    auto rCardB = Cards::reverseCard{'b'};
-    auto rCardC = Cards::reverseCard{'a'};
+    Cards::reverseCard rCardA = Cards::reverseCard{'a'};
+    Cards::reverseCard rCardB = Cards::reverseCard{'b'};
+    Cards::reverseCard rCardC = Cards::reverseCard{'a'};
 
     EXPECT_NE(rCardA, rCardB);
     EXPECT_TRUE(rCardA.sameNumber(rCardC));
@@ -53,9 +52,9 @@ TEST(Cards, reverseCardComparison)
 
 TEST(Cards, skipCardComparison)
 {
-    auto rCardA = Cards::skipCard{'a'};
-    auto rCardB = Cards::skipCard{'b'};
-    auto rCardC = Cards::skipCard{'a'};
+    Cards::skipCard rCardA = Cards::skipCard{'a'};
+    Cards::skipCard rCardB = Cards::skipCard{'b'};
+    Cards::skipCard rCardC = Cards::skipCard{'a'};
 
     EXPECT_NE(rCardA, rCardB);
     EXPECT_TRUE(rCardA.sameNumber(rCardC));
