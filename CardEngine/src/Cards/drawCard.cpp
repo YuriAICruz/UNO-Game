d1de83fd03f2e2d42eb5389eb1484dde7a7aab43
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-05 17:33:09 -0300

    draw card class

---
+++ CardEngine/src/Cards/drawCard.cpp
@@ -0,0 +1,5 @@
+﻿#include "pch.h"
+#include "drawCard.h"
+
+namespace Cards
+{}
---
+++ CardEngine/src/Cards/drawCard.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include <vcruntime_typeinfo.h>
+
+#include "ICard.h"
+#include "../../framework.h"
+
+namespace Cards
+{
+    class ENGINE_API drawCard : public ICard
+    {
+    public:
+        explicit drawCard(int drawCount, char color)
+        {
+            this->number = drawCount;
+            this->color = color;
+        }
+        
+        bool equal(const ICard& other) const override
+        {
+            if (color != other.Color())
+            {
+                return false;
+            }
+
+            if(typeid(drawCard) == typeid(other))
+            {
+                return number == other.Number();
+            }
+
+            return false;
+        }
+
+        bool sameNumber(ICard& other) const override
+        {
+            return false;
+        }
+    };
+}
