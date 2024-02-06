1df61b0a91bead6d61b6a50fbbe42cb722b0535f
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-05 21:54:09 -0300

    create card factory class

---
+++ CardEngine/src/Cards/cardFactory.cpp
@@ -0,0 +1,40 @@
+﻿#include "pch.h"
+#include "cardFactory.h"
+
+#include <memory>
+
+#include "baseCard.h"
+#include "drawCard.h"
+#include "reverseCard.h"
+#include "skipCard.h"
+
+namespace Cards
+{
+    std::vector<std::unique_ptr<ICard>> cardFactory::instances;
+
+    ICard* cardFactory::Instantiate(const char* value, int number, char color)
+    {
+        if (strcmp(drawCard::TypeId(), value) == 0)
+        {
+            auto up = std::make_unique<drawCard>(number, color);
+            instances.push_back(std::move(up));
+            return instances.back().get();
+        }
+        if (strcmp(reverseCard::TypeId(), value) == 0)
+        {
+            auto up = std::make_unique<reverseCard>(color);
+            instances.push_back(std::move(up));
+            return instances.back().get();
+        }
+        if (strcmp(skipCard::TypeId(), value) == 0)
+        {
+            auto up = std::make_unique<skipCard>(color);
+            instances.push_back(std::move(up));
+            return instances.back().get();
+        }
+
+        auto up = std::make_unique<baseCard>(number, color);
+        instances.push_back(std::move(up));
+        return instances.back().get();
+    }
+}
---
+++ CardEngine/src/Cards/cardFactory.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+#include <memory>
+#include <vector>
+
+#include "ICard.h"
+
+namespace Cards
+{
+    class cardFactory
+    {
+    private:
+        static std::vector<std::unique_ptr<ICard>> instances;
+    public:
+        static ICard* Instantiate(const char* value, int number, char color);
+    };
+}
