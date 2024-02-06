5ce0f4a6621af9cffc0100a2523f121fe592ec3a
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-06 16:51:06 -0300

    event bus creation and tests

---
+++ CardEngine/src/EventBus/abstractEventBase.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+namespace eventBus
+{
+    struct abstractEventBase
+    {
+        virtual ~abstractEventBase() = default;
+    };
+}
---
+++ CardEngine/src/EventBus/eventBus.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+#include <map>
+#include <memory>
+
+#include "alreadyExistException.h"
+#include "event.h"
+
+namespace eventBus
+{
+    struct abstractEventBase;
+
+    class eventBus
+    {
+    private:
+        std::map<int, std::unique_ptr<abstractEventBase>> events;
+
+    public:
+        template <typename T>
+        void bindEvent(int id)
+        {
+            if (!events.count(id))
+            {
+                events.emplace(id, std::make_unique<event<T>>());
+                return;
+            }
+
+            throw new alreadyExistException();
+        }
+
+        template <typename T>
+        void subscribe(int id, std::function<void(T)> func)
+        {
+            static_cast<event<T>*>(events.at(id).get())->subscribe(func);
+        }
+
+        template <typename T>
+        void fireEvent(int id, T data)
+        {
+            static_cast<event<T>*>(events.at(id).get())->fire(data);
+        }
+    };
+}
