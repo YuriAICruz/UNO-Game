b976ba57ba9204f1d307df856dc3c827e945521c
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-05 21:56:05 -0300

    deck logic implementation

--- CardEngine/src/Decks/deck.cpp
+++ CardEngine/src/Decks/deck.cpp
@@ -1,40 +1,79 @@
 ﻿#include "pch.h"
 #include "deck.h"
 
-namespace Decks{
+#include <fstream>
+#include <iostream>
+#include <random>
+
+#include "../../json.hpp"
+#include "../Cards/baseCard.h"
+#include "../Cards/cardFactory.h"
+
+using json = nlohmann::json;
+
+namespace Decks
+{
     deck::deck(const char* jsonFilePath)
     {
+        std::ifstream file(jsonFilePath);
+        json jsonData;
+        file >> jsonData;
+        file.close();
+
+        for (auto data : jsonData)
+        {
+            auto t = data.contains("type") ? data["type"].get<std::string>().c_str() : "";
+            int copies = data.contains("repeat") ? data["repeat"].get<int>() : 1;
+            for (int i = 0; i < copies; ++i)
+            {
+                Cards::ICard* card = Cards::cardFactory::Instantiate(
+                    t,
+                    data["number"],
+                    data["color"].get<std::string>()[0]
+                );
+                cards.push_back(card);
+            }
+        }
     }
 
     unsigned int deck::count()
     {
-        return 0;
+        return cards.size();
     }
 
     Cards::ICard* deck::peek()
     {
-        return nullptr;
+        return cards.front();
     }
 
     void deck::shuffle()
     {
+        std::vector<Cards::ICard*> v(cards.begin(), cards.end());
+        std::random_device rng;
+        std::mt19937 generator(rng());
+        std::shuffle(v.begin(), v.end(), generator);
+        cards.assign(v.begin(), v.end());
     }
 
     Cards::ICard* deck::peekLast()
     {
-        return nullptr;
+        return cards.back();
     }
 
     void deck::stack(Cards::ICard* card)
     {
+        return cards.push_front(card);
     }
 
     void deck::enqueue(Cards::ICard* card)
     {
+        return cards.push_back(card);
     }
 
     Cards::ICard* deck::dequeue()
     {
-        return nullptr;
+        auto card = cards.front();
+        cards.pop_front();
+        return card;
     }
 }
