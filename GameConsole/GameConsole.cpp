ea0691f6fe835b66731765a2ef2354ac06d9c5b8
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-07 19:10:57 -0300

    try to set and get window size

--- GameConsole/GameConsole.cpp
+++ GameConsole/GameConsole.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
-#include "temp.h"
+
+#include "Cards/ICard.h"
 
 int main()
 {
-    std::cout << temp::helloWorld() << "\n";
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    SMALL_RECT windowSize = {0, 0, 79, 24};
+    SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
+
+    COORD bufferSize = {80, 25};
+    SetConsoleScreenBufferSize(hConsole, bufferSize);
+
+    system("cls");
+
+    CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
+    GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
+    int windowWidth = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
+    int windowHeight = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top +1;
+
+    std::cout << windowWidth << "," << windowHeight << "\n";
+    for (int y = 0; y < windowHeight; ++y)
+    {
+        for (int x = 0; x < windowWidth; ++x)
+        {
+            std::cout << "*";
+        }
+        std::cout << "\n";
+    }
+
+    std::string str;
+    std::cin >> str;
+    return 0;
 }
