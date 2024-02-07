9065e1c052b0d83e486fdba2bcca0d8f8fa35225
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-07 13:15:45 -0300

    turnSystem namespace refactoring

--- CardEngine/src/StateManager/gameStateManager.h
+++ CardEngine/src/StateManager/gameStateManager.h
@@ -7,7 +7,7 @@ class gameStateManager : IStateManager
 {
 public:
     virtual void startGame() = 0;
-    virtual TurnSystem::IPlayer* getCurrentPlayer() const = 0;
+    virtual turnSystem::IPlayer* getCurrentPlayer() const = 0;
     virtual cards::ICard* getTopCard() const = 0;
     virtual bool tryExecutePlayerAction(cards::ICard* card) = 0;
 };
