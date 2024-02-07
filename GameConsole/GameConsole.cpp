9155aaeee7b34c435c802257cddc88a67b363058
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-07 19:28:05 -0300

    redraw on rescale

--- GameConsole/GameConsole.cpp
+++ GameConsole/GameConsole.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
+#include <Windows.h>
 
-#include "Cards/ICard.h"
+HANDLE hConsole;
 
-int main()
+void redraw(COORD size)
 {
-    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-
-    COORD bufferSize = {80, 25};
-    SetConsoleScreenBufferSize(hConsole, bufferSize);
-
     system("cls");
 
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     GetConsoleScreenBufferInfo(hConsole, &consoleInfo);
-    int windowWidth = consoleInfo.srWindow.Right - consoleInfo.srWindow.Left + 1;
-    int windowHeight = consoleInfo.srWindow.Bottom - consoleInfo.srWindow.Top + 1;
+    int windowWidth = size.X;
+    int windowHeight = size.Y;
 
     std::cout << windowWidth << "," << windowHeight << "\n";
     for (int y = 0; y < windowHeight; ++y)
@@ -25,8 +21,53 @@ int main()
         }
         std::cout << "\n";
     }
+}
 
-    std::string str;
-    std::cin >> str;
+COORD GetConsoleWindowSize()
+{
+    CONSOLE_SCREEN_BUFFER_INFO info;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
+    SHORT x = info.srWindow.Right - info.srWindow.Left + 1;
+    SHORT y = info.srWindow.Bottom - info.srWindow.Top + 1;
+    COORD size = COORD{x, y};
+    return size;
+}
+
+BOOL WINAPI ConsoleHandlerRoutine(DWORD dwCtrlType)
+{
+    std::cout << dwCtrlType << std::endl;
+    return FALSE;
+}
+
+int main()
+{
+    if (!SetConsoleCtrlHandler(ConsoleHandlerRoutine, TRUE))
+    {
+        std::cerr << "Error setting up console handler." << std::endl;
+        return 1;
+    }
+
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    COORD bufferSize = {80, 25};
+    SetConsoleScreenBufferSize(hConsole, bufferSize);
+
+    COORD initialSize = GetConsoleWindowSize();
+    redraw(initialSize);
+
+    while (true)
+    {
+        COORD currentSize = GetConsoleWindowSize();
+
+        // Compare current size with initial size
+        if (currentSize.X != initialSize.X || currentSize.Y != initialSize.Y)
+        {
+            // Window resized
+            std::cout << "Window resized." << std::endl;
+            // Update initial size
+            initialSize = currentSize;
+            redraw(initialSize);
+        }
+    }
     return 0;
 }
