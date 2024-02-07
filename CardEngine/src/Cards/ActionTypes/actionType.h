4ff97df23f0405c821429a715350eda0501d6791
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-07 12:40:25 -0300

    card action types

---
+++ CardEngine/src/Cards/ActionTypes/actionType.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+namespace cards
+{
+    namespace actions
+    {
+        class actionType
+        {
+        public:
+            virtual ~actionType() = default;
+            virtual bool isEqual(const type_info& actionType) = 0;
+        };
+    }
+}
---
+++ CardEngine/src/Cards/ActionTypes/draw.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "actionType.h"
+#include <typeinfo>
+
+namespace cards
+{
+    namespace actions
+    {
+        class draw : public actionType
+        {
+        public:
+            bool isEqual(const type_info& actionType) override
+            {
+                return actionType.hash_code() == typeid(draw).hash_code();
+            }
+    
+        };
+    }
+}
