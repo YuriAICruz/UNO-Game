c8e456bdfe05943e51fa3fc4416af87bda34f711
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-22 21:00:18 -0300

    update definitions

--- CardEngine/framework.h
+++ CardEngine/framework.h
@@ -1,13 +1,12 @@
 #pragma once
 
+#define CARDENGINE_EXPORTS
 
-#define API_EXPORTS
-
-#ifdef API_EXPORTS
+#ifdef CARDENGINE_EXPORTS
 #define ENGINE_API __declspec(dllexport)
 #else
 #define ENGINE_API __declspec(dllimport)
-#endif //API_EXPORTS
+#endif // CARDENGINE_EXPORTS
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
--- MultiplayerCardEngine/framework.h
+++ MultiplayerCardEngine/framework.h
@@ -1,12 +1,10 @@
 #pragma once
 
-#define API_EXPORTS
-
-#ifdef API_EXPORTS
+#ifdef MCARD_ENGINE_EXPORTS
 #define NET_ENGINE_API __declspec(dllexport)
 #else
 #define NET_ENGINE_API __declspec(dllimport)
-#endif //API_EXPORTS
+#endif //MCARD_ENGINE_EXPORTS
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
 // Windows Header Files
