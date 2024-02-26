389aeb4085b86552afb4e531e39e85c27f457d49
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-26 12:36:24 -0300

    has room cmd

---
+++ NetLib/src/commands/client/hasRoomCmd.cpp
@@ -0,0 +1,24 @@
+﻿#include "hasRoomCmd.h"
+
+#include "getRoomCmd.h"
+#include "../../client/client.h"
+
+namespace commands
+{
+    bool hasRoomCmd::execute()
+    {
+        netcode::room* r;
+        pending = false;
+
+        if (netClient->executeCommand<getRoomCmd>(r))
+        {
+            return r->count() > 0;
+        }
+
+        return false;
+    }
+
+    void hasRoomCmd::callback(const std::string& message)
+    {
+    }
+}
