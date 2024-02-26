6dacbd5361ebc95f8a98b0cf4fd7e385dfeb177c
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-26 12:35:48 -0300

    get room cmd

---
+++ NetLib/src/commands/client/getRoomCmd.cpp
@@ -0,0 +1,17 @@
+﻿#include "getRoomCmd.h"
+
+#include "../../client/client.h"
+
+namespace commands
+{
+    bool getRoomCmd::execute()
+    {
+        roomRef = &netClient->currentRoom;
+        pending = false;
+        return true;
+    }
+
+    void getRoomCmd::callback(const std::string& message)
+    {
+    }
+}
---
+++ NetLib/src/commands/client/getRoomCmd.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include "clientCommand.h"
+#include "../../serverCommands.h"
+#include "../../server/room.h"
+
+namespace commands
+{
+    class NETCODE_API getRoomCmd : public clientCommand
+    {
+        netcode::room*& roomRef;
+
+    public:
+        getRoomCmd(netcode::room*& room, netcode::client* client)
+            : clientCommand(NC_NO_CALLBACK, client), roomRef(room)
+        {
+        }
+
+        bool execute() override;
+        void callback(const std::string& message) override;
+    };
+}
