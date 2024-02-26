27e99b3fee1c400353114cbcc81ee7a94a41e915
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-26 12:35:56 -0300

    get rooms cmd

---
+++ NetLib/src/commands/client/getRoomsCmd.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include "clientCommand.h"
+#include "../../serverCommands.h"
+#include "../../server/room.h"
+
+namespace commands
+{
+    class NETCODE_API getRoomsCmd : public clientCommand
+    {
+    private:
+        std::vector<netcode::room>& roomList;
+
+    public:
+        getRoomsCmd(std::vector<netcode::room>& roomList, netcode::client* client)
+            : clientCommand(NC_LIST_ROOMS, client), roomList(roomList)
+        {
+        }
+
+        bool execute() override;
+        void callback(const std::string& message) override;
+    };
+}
