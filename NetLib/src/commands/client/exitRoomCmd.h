bfa55ac45187315901a52a3de7a6bbaab9c39ad6
Yuri Cruz <yuriaicruz@gmail.com>
2024-02-26 12:35:43 -0300

    exit room cmd

---
+++ NetLib/src/commands/client/exitRoomCmd.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "clientCommand.h"
+#include "../../serverCommands.h"
+
+namespace commands
+{
+    class NETCODE_API exitRoomCmd : public clientCommand
+    {
+    public:
+        exitRoomCmd(netcode::client* client)
+            : clientCommand(NC_EXIT_ROOM, client)
+        {
+        }
+
+        bool execute() override;
+        void callback(const std::string& message) override;
+    };
+}
